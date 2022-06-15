5dca5a83e2ba70038d668eafba5763a1dc1c0741
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-06-15 13:41:45 -0700

    Create TimedComputation.java

---
+++ src/TimedComputation.java
@@ -0,0 +1,17 @@
+/*
+ *  This program performs some mathematical computations and displays the
+ *  results. It also displays the value of the constant MAth.PI. It then 
+ *  reports the number of seconds that the computer spent on the task.
+ */
+
+ public class TimedComputation{
+    
+    public static void main(String[] args) {
+        long startTime;   // Starting time of program, in nanoseconds
+        long endTime;     // Time when computations are done, in nano seconds
+        long compTime;    // Run time in nano seconds
+        double seconds;   // Time difference, in seconds
+
+        System.out.println("Hello World");
+    }   // End of PSV Main
+ }   // End of TimedComputation
